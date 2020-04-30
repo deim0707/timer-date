758c90ac418401f867d0b4f80635ea0ac328ce42
deim0707 <dima1parshin@gmail.com>
2020-05-01 02:50:15 +0300

    Показывать разницу в днях, между введёной и нынешней датой

--- project/script.js
+++ project/script.js
@@ -5,14 +5,25 @@ const dateValue = document.getElementById('date-value');
 const takeDate = document.getElementById('take-date');
 const dateResult = document.getElementById('date-result');
 
+const dateNow = new Date;
+let newDate; //сюда запишется дата введёная пользователем
+let differenceInDays; //сюда запишется разница между введёной датой и нынешней
+
+//функция для создания новой даты //пригодится в будущем
+const makeNewDate = (year, month, date, hours=0, minutes=0) => new Date(year, month, date, hours, minutes);
+//показывает разницу между датами в днях
+const makeDifferenceInDays = (date1, date2) =>  Math.ceil(Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
 
 
 //происходит по клику на кнопку "Получить дату"
-takeDate.onclick = function () {
-    if(dateValue.value.length === 0) dateResult.textContent='Введите, пожалуйста, дату'
-    else dateResult.textContent=dateValue.value
+takeDate.onclick = () => {
+    if(dateValue.value.length === 0) dateResult.textContent='Введите, пожалуйста, дату'; //проверка, введена ли дата
+    else {
+        newDate = new Date (dateValue.value)
+        differenceInDays=makeDifferenceInDays(newDate, dateNow);
+        c(`Сейчас: ${dateNow}. Введённая дата: ${newDate}. Разница между ними в днях: ${differenceInDays}`)
+        dateResult.textContent=`Разница в днях между датами: ${differenceInDays}`;
+    }
 }
 
-const newTime = (year, month, date, hours=0, minutes=0) => new Date(year, month, date, hours, minutes);
 
-c(newTime(2012, 10, 11))

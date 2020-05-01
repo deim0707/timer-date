c9a297f75698524e9852a4d33c536048b007f1b2
deim0707 <dima1parshin@gmail.com>
2020-05-01 06:06:19 +0300

    добавил заготовку под таймер, сейчас это часы

--- project/script.js
+++ project/script.js
@@ -9,10 +9,9 @@ const dateNow = new Date;
 let newDate; //сюда запишется дата введёная пользователем
 let differenceInDays; //сюда запишется разница между введёной датой и нынешней
 
-//функция для создания новой даты //пригодится в будущем
-const makeNewDate = (year, month, date, hours=0, minutes=0) => new Date(year, month, date, hours, minutes);
+
 //показывает разницу между датами в днях
-const makeDifferenceInDays = (date1, date2) =>  Math.ceil(Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
+const getDifferenceInDays = (date1, date2) =>  Math.ceil(Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
 
 
 //происходит по клику на кнопку "Получить дату"
@@ -20,10 +19,44 @@ takeDate.onclick = () => {
     if(dateValue.value.length === 0) dateResult.textContent='Введите, пожалуйста, дату'; //проверка, введена ли дата
     else {
         newDate = new Date (dateValue.value)
-        differenceInDays=makeDifferenceInDays(newDate, dateNow);
+        differenceInDays=getDifferenceInDays(newDate, dateNow);
         c(`Сейчас: ${dateNow}. Введённая дата: ${newDate}. Разница между ними в днях: ${differenceInDays}`)
         dateResult.textContent=`Разница в днях между датами: ${differenceInDays}`;
     }
 }
 
 
+
+
+//ЗАГОТОВКИ
+//функция для создания новой даты
+const getNewDate = (year, month, date, hours=0, minutes=0) => new Date(year, month, date, hours, minutes);
+
+//заготовка под таймер
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const checkTime = (date, idElement) => {
+    var date = date;
+    var sufix = '';
+    var hours = ('0' + date.getHours()).slice(-2);
+    var minutes = ('0' + date.getMinutes()).slice(-2);
+    var day = date.getDate();
+    var month = monthNames[date.getMonth()];
+    var weekday = dayNames[date.getDay()];
+    if (day > 3 && day < 21) sufix = 'th';
+    switch (day % 10) {
+      case 1:
+        sufix = "st";
+      case 2:
+        sufix = "nd";
+      case 3:
+        sufix = "rd";
+      default:
+        sufix = "th";
+    }
+    document.getElementById(idElement).innerHTML = "  Сейчас <span class='hour'>" + hours + ":" + minutes + "</span><br/><span class='date'>" + month + ' ' + day + sufix + ', ' + weekday + '.';
+}
+
+checkTime(dateNow, 'timer')

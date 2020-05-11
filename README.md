d3f48dc060c78b8a35660a58d56c947cc5f189e8
Vladimir Nickolaev <exlnted@gmail.com>
2020-05-11 17:57:25 +0300

    запилил удаление

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# <a href='https://deim0707.github.io/timer-date/project/index.html'>Посмотреть</a>
+# <a href='https://deim0707.github.io/timer-date/index.html'>Посмотреть</a>
+
+задание:
+Таймер обратного отсчета
+Уровень: 1-новичок
+Целью таймера обратного отсчета является предоставление непрерывно уменьшающегося отображения месяцев, дней, часов, минут и секунд для введенного пользователем события.
+
+Пользовательские истории
+- Пользователь может видеть поле ввода события, содержащее поле имени события, поле даты, дополнительное время и кнопку «Пуск».
+- Пользователь может определить событие, введя его имя, дату, на которую оно запланировано, и дополнительное время события. Если время не указано, предполагается, что оно будет в полночь даты события в местном часовом поясе.
+- Пользователь может увидеть предупреждающее сообщение, если имя события пустое.
+- Пользователь может увидеть предупреждение, если дата или время события введены неправильно.
+- Пользователь может увидеть предупреждающее сообщение, если время до тех пор, пока введенные данные события и время не превысят точность таймера обратного отсчета.
+- Пользователь может нажать на кнопку «Пуск», чтобы увидеть, как таймер обратного отсчета начинает отображать дни, часы, минуты и секунды до наступления события.
+- Пользователь может видеть элементы таймера обратного отсчета автоматически уменьшаться. Например, когда счетчик оставшихся секунд достигнет 0, счетчик оставшихся минут уменьшится на 1, и отсчет секунд начнется с 59. Эта последовательность должна происходить от секунд до позиции оставшихся дней на дисплее обратного отсчета.
+Бонусные функции
+- Пользователь может сохранить событие, чтобы оно сохранялось между сеансами
+- Пользователь может увидеть предупреждение при достижении события
+- Пользователь может указать более одного события.
+- Пользователь может видеть таймеры обратного отсчета для каждого определенного события.
+/////////////////////////////////////
+Countdown Timer
+Tier: 1-Beginner
+We all have important events we look forward to in life, birthdays, anniversaries, and holidays to name a few. Wouldn’t it be nice to have an app that counts down the months, days, hours, minutes, and seconds to an event? Countdown Timer is just that app!
+The objective of Countdown Timer is to provide a continuously decrementing display of the he months, days, hours, minutes, and seconds to a user entered event.
+Constraints
+- Use only builtin language functions for your calculations rather than relying on a library or package like MomentJS. This assumes, of course, that the language of your choice has adequate date and time manipulation functions built in.
+- You may not use any code generators such as the Counting Down To site. You should develop your own original implementation.
+User Stories
+- User can see an event input box containing an event name field, an date field, an optional time, and a ‘Start’ button.
+- User can define the event by entering its name, the date it is scheduled to take place, and an optional time of the event. If the time is omitted it is assumed to be at Midnight on the event date in the local time zone.
+- User can see a warning message if the event name is blank.
+- User can see a warning message if the event date or time are incorrectly entered.
+- User can see a warning message if the time until the event data and time that has been entered would overflow the precision of the countdown timer.
+- User can click on the ‘Start’ button to see the countdown timer start displaying the days, hours, minutes, and seconds until the event takes place.
+- User can see the elements in the countdown timer automatically decrement. For example, when the remaining seconds count reaches 0 the remaining minutes count will decrement by 1 and the seconds will start to countdown from 59. This progression must take place from seconds all the way up to the remaining days position in countdown display.
+Bonus features
+- User can save the event so that it persists across sessions
+- User can see an alert when the event is reached
+- User can specify more than one event.
+- User can see a countdown timers for each event that has been defined.
---
+++ src/script.js
@@ -0,0 +1,176 @@
+const c = console.log;
+
+const dateValue = document.getElementById('date-value'); //поле ввода даты события
+const nameValue = document.getElementById('name-value'); //поле ввода имени события
+const timeValue = document.getElementById('input-time'); //поле ввода времени
+const makeDate = document.getElementById('take-date'); //кнопка Пуск
+const dateResult = document.getElementById('date-result'); //сюда добавляем таймеры
+const info = document.getElementById('info'); //место, куда можно выводить информационные сообщения
+const checkboxTime = document.getElementById('checkbox-time'); //чекбокс, отображения\скрытия времени
+
+let interval; //переменная в общем поле видимости, которая позволит очистить интервал
+
+//делает информационное сообщение на заданое кол-во мс
+const makeInfoMessage = (message, delay) => {
+	info.innerHTML += `<div><b> ${message} </b></div>`;
+	setTimeout(() => (info.innerHTML = null), delay);
+};
+//происходит при нажатии на чекбокс "показать время". скрывает и показывает поле ввода времени
+checkboxTime.onchange = (event) => {
+	event.target.checked === false ? timeValue.classList.add('hidden') : timeValue.classList.remove('hidden');
+};
+//функция для разблокировки кнопки Пуск, когда ввели Дату и Имя события
+const checkContent = (...args) => {
+	args.forEach((arg) => {
+		arg.oninput = () => {
+			if (dateValue.value.length && nameValue.value.length) {
+				makeDate.disabled = false;
+			}
+		};
+	});
+};
+checkContent(dateValue, nameValue);
+
+const timeFormatter = (date) => {
+	const ms = Number(date);
+	let delta = Math.abs(date) / 1000; //переводит дату в секунды
+	const years = Math.floor(delta / 31536000); //сколько лет влазит в это кол-во секунд
+	delta -= years * 31536000; //получаем остаток секунд, вычитая из него кол-во милисекунд в целых годах
+	const months = Math.floor(delta / 2592000); //сколько месяцев влазит в остаток милисекунд после подсчёта Года
+	delta -= months * 2592000;
+	const days = Math.floor(delta / 86400);
+	delta -= days * 86400;
+	const hours = Math.floor(delta / 3600) % 24;
+	delta -= hours * 3600;
+	const minutes = Math.floor(delta / 60) % 60;
+	delta -= minutes * 60;
+	const seconds = Math.floor(delta % 60);
+	return {
+		years: years,
+		months: months,
+		days: days,
+		hours: hours,
+		minutes: minutes,
+		seconds: seconds,
+		ms: ms,
+	};
+};
+
+class Event {
+	constructor(name, date, time) {
+		this._id = Event.counter;
+		this.name = name;
+		this.newDate = new Date(`${date}T${time || '00:00:00'}`);
+	}
+
+	static get counter() {
+		//счётчик для id
+		Event._counter = Date.now();
+		return Event._counter;
+	}
+}
+const getEventFromStorage = (key) =>
+	JSON.parse(localStorage.getItem(key)) === null ? [] : JSON.parse(localStorage.getItem(key));
+const setToStorage = (key, arrWithItems) => localStorage.setItem(key, JSON.stringify(arrWithItems));
+const addToStorage = (key, item) => setToStorage(key, [...getEventFromStorage(key), item]);
+
+// localStorage.clear()
+// если localstorage пуст (запустили первый раз. то внесём в него 3 события)
+if (localStorage === null) {
+	setToStorage('events', [new Event('Новый год', '2020-12-31')]);
+	addToStorage('events', new Event('Начало летa', '2020-06-01', '11:30:30'));
+	addToStorage('events', new Event('8 мая 11:30', '2020-05-08', '11:30:30'));
+}
+
+const renderEventTemplate = (nameEvent, eventDifference, isPresent, id) => {
+	return {
+		template: `
+    <b>Событие:</b> ${nameEvent}. 
+    <b>${isPresent ? ' До него:' : 'C тех пор прошло:'}</b>   
+    ${eventDifference.years !== 0 ? eventDifference.years + ' лет,' : ''}  
+    ${eventDifference.months !== 0 ? eventDifference.months + ' месяцев,' : ''} 
+    ${eventDifference.days !== 0 ? eventDifference.days + ' дней,' : ''} 
+    ${eventDifference.hours !== 0 ? eventDifference.hours + ' часов,' : ''} 
+    ${eventDifference.minutes + ' минут,'} 
+    ${eventDifference.seconds + ' секунд'} 
+    <button id=${id.toString()}>Удолить событие</button>
+    `,
+		id,
+	};
+};
+const renderEvents = (arr) => {
+	if (arr === null) makeInfoMessage('Список событий пуст', 60000);
+	else {
+		let arrForTeg = []; //массив, где будут лежать результаты createElement
+
+		arr.forEach((event, idx) => {
+			arrForTeg.push(document.createElement('p')); //создаём для каждого элемента массива тэг
+			let itemDifference = timeFormatter(new Date(event.newDate) - new Date()); //каждую секунду пересчитывается разница во времени между заданной датой и нынешней
+			let result;
+			let deleteButtom;
+			//для каждого созданного тега делаем текстовое содержимое
+			if (itemDifference.ms > 0) result = renderEventTemplate(event.name, itemDifference, true, event._id);
+			arrForTeg[idx].innerHTML = result.template;
+
+			// deleteButtom = document.getElementById(result.id.toString()); тут кнопка еще не доступна
+
+			//если таймер кончился
+			if (itemDifference.ms <= 0)
+				arrForTeg[idx].innerHTML = renderEventTemplate(event.name, itemDifference, false, event._id);
+
+			dateResult.appendChild(arrForTeg[idx]); //вставляем на страницу
+		});
+		//каждую секунду для каждого эл-та понижаем кол-во милисекунд на 1000
+		interval = setInterval(() => {
+			arr.forEach((event, idx) => {
+				let itemDifference = timeFormatter(new Date(event.newDate) - 1000 - new Date());
+				let result = renderEventTemplate(event.name, itemDifference, true, event._id);
+				let deleteButtom = document.getElementById(result.id.toString());
+
+				// event.difference = countDownTimer(event.difference.ms); //вычитаем 1000 милисекунд и возвращаем новое отформатированное значение
+				if (itemDifference.ms > 0) {
+					arrForTeg[idx].innerHTML = '';
+					arrForTeg[idx].innerHTML = result.template;
+					deleteButtom = document.getElementById(result.id.toString());
+					deleteButtom.addEventListener('click', (e) => {
+						e.preventDefault();
+						dateResult.removeChild(arrForTeg[idx]);
+						arr = arr.filter((el) => el._id !== result.id);
+						localStorage.setItem('events', JSON.stringify(arr));
+					});
+				}
+				//если таймер кончился
+				if (itemDifference.ms <= 0)
+					arrForTeg[idx].innerHTML = renderEventTemplate(event.name, itemDifference, false);
+			});
+			// console.log('интервал сработал')
+		}, 1000);
+	}
+};
+const addNewEventInRender = (event) => {
+	addToStorage('events', event);
+	dateResult.textContent = null;
+	clearInterval(interval);
+	renderEvents(getEventFromStorage('events')); //снова отрендерили это всё
+};
+renderEvents(getEventFromStorage('events'));
+
+const deleteEventFromRender = (key, id) => {
+	setToStorage(
+		key,
+		getEventFromStorage(key).filter((item) => item._id !== id)
+	);
+	dateResult.textContent = null;
+	clearInterval(interval);
+	renderEvents(getEventFromStorage(key));
+};
+// deleteEventFromRender('events',4);
+
+makeDate.onclick = (e) => {
+	e.preventDefault();
+	addNewEventInRender(new Event(nameValue.value, dateValue.value, timeValue.value));
+	dateValue.value = '';
+	nameValue.value = '';
+	timeValue.value = '';
+	makeDate.disabled = true;
+};
